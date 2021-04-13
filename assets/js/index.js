64cc0265a6c821a9587d1af52fa28b76f3383a90
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-04-13 19:35:42 +0100

    added function to remove search container and add search to nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,26 @@
+// function to remove search container and append main sections
+const removeSearchAndAppendMain = async () => {
+  //remove search container
+  $("#search-container").remove();
+
+  // add search bar to navbar
+  $("#navbar").prepend(`<div class="item">
+  <div class="ui fluid search">
+    <div class="ui icon input" id="search-nav"><input type="text" placeholder="Enter country..." id="search-bar" />
+    <i class="search icon"></i></div>
+  </div>
+`);
+};
+
 // function called on submit of search form
 const onSubmit = async (event) => {
   event.preventDefault();
 
   const countryName = $("#search-bar").val();
   console.log(countryName);
+
+  // remove search container and append search results container
+  removeSearchAndAppendMain();
 };
 
 $("#start-form").on("submit", onSubmit);
