fb931e2d08c9c25fdde57e788889c8f52889c3a6
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-04-13 21:02:23 +0100

    added control flow for fetch calls and tidied up search bar

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,37 +4,54 @@ const fetchData = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
     return data;
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //function to build URL for REST countries to get data for country card
-const createCountryCardUrl = (countryName) => {
-  return `https://restcountries.eu/rest/v2/name/${countryName}`;
-};
+const createCountryCardUrl = (countryName) =>
+  `https://restcountries.eu/rest/v2/name/${countryName}`;
 
 // extract needed data from REST countries api call to construct country card
 const getCountryCardData = async (restApiData) => {
+  const data = restApiData[0];
   return {
-    name: await restApiData[0].name,
-    flag: await restApiData[0].flag,
-    capital: await restApiData[0].capital,
-    language: await restApiData[0].languages[0].name,
-    currency: await restApiData[0].currencies[0].name,
+    name: await data.name,
+    flag: await data.flag,
+    capital: await data.capital,
+    language: await data.languages[0].name,
+    currency: await data.currencies[0].name,
   };
 };
 
 // function to remove search container and append main sections
-const removeSearchAndAppendMain = async (restApiData) => {
+const removeSearchAndAppendMain = async () => {
   //remove search container
   $("#search-container").remove();
 
   // add search bar to navbar
-  $("#navbar").prepend(`<div class="item">
-  <div class="ui fluid search">
-    <div class="ui icon input" id="search-nav"><input type="text" placeholder="Enter country..." id="search-bar" />
-    <i class="search icon"></i></div>
+  $("#navbar").prepend(`
+  <div class="item">
+    <form id="nav-form">
+      <div class="field">
+        <div class="ui fluid search">
+          <div class="ui icon input" id="search-nav">
+            <input
+              type="text"
+              placeholder="Enter country..."
+              id="search-bar"
+            />
+            <button type="submit">
+            <i class="search icon"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
   </div>
 `);
+  $("#nav-form").on("submit", onSubmit);
 };
 
 // function called on submit of search form
@@ -42,16 +59,19 @@ const onSubmit = async (event) => {
   event.preventDefault();
 
   const countryName = $("#search-bar").val();
-  console.log(countryName);
 
-  // create URL + fetch data for country card
-  const urlForCountryCard = createCountryCardUrl(countryName);
-  const restApiData = await fetchData(urlForCountryCard);
-  const countryCardData = await getCountryCardData(restApiData);
-  console.log(countryCardData);
-
-  // remove search container and append search results container
-  removeSearchAndAppendMain(restApiData);
+  if (countryName) {
+    // create URL + fetch data for country card
+    const urlForCountryCard = createCountryCardUrl(countryName);
+    const restApiData = await fetchData(urlForCountryCard);
+    if (Array.isArray(restApiData)) {
+      const countryCardData = await getCountryCardData(restApiData);
+      if (event.target.id === "start-form") {
+        // remove search container and append search results container
+        removeSearchAndAppendMain();
+      }
+    }
+  }
 };
 
 $("#start-form").on("submit", onSubmit);
