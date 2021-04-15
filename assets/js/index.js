fb7d58b7834ef8c96d52a5f748c1e2f2fd3b2ed4
Otilia Visan <otilia.visan22@gmail.com>
2021-04-15 21:07:29 +0100

    created url for list items

--- assets/js/index.js
+++ assets/js/index.js
@@ -140,6 +140,11 @@ const getCountryCardData = async (restApiData) => {
 //function to build URL for REST countries to get data for country card
 const createPlacesCardUrl = (data, apiKey) =>
   `https://api.opentripmap.com/0.1/en/places/geoname?apikey=${apiKey}&name=${data.capital}`;
+
+//function to build URL for REST countries to get data for country card
+const createListItemsUrl = (data, apiKey, offset, pageLength) =>
+  `https://api.opentripmap.com/0.1/en/places/radius?apikey=${apiKey}&radius=1000&limit=${pageLength}&offset=${offset}&lon=${data.lon}&lat=${data.lat}&rate=2&format=json`;
+
 // function to remove search container and append main sections
 const removeSearchAndAppendMain = async () => {
   //remove search container
@@ -214,6 +219,15 @@ const onSubmit = async (event) => {
       const apiKey = "5ae2e3f221c38a28845f05b6fac16143ca6a7e70223b17f1cc98d3e7";
       const urlForPlacesCard = createPlacesCardUrl(countryCardData, apiKey);
       const placesData = await fetchData(urlForPlacesCard);
+      let offset = 0;
+      const pageLength = 5;
+      const urlForListItems = createListItemsUrl(
+        placesData,
+        apiKey,
+        offset,
+        pageLength
+      );
+      console.log(urlForListItems);
       if (event.target.id === "start-form") {
         // remove search container and append search results container
         removeSearchAndAppendMain();
