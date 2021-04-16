7defba644a6af11f898a6d458b5d9bada267cd99
Pamela <pamela_bhopal@hotmail.co.uk>
2021-04-16 20:15:26 +0100

    created function to render favourites cards

--- assets/js/favourites.js
+++ assets/js/favourites.js
@@ -7,10 +7,22 @@ const getFromLocalStorage = () => {
   }
 };
 
-const onLoad = () => {
-  getFromLocalStorage();
+const renderFavCountryCard = (item) => {
+  console.log(item);
+};
 
-  // renderFavouritesCards();
+const renderFavouritesCards = (favourites) => {
+  if (favourites.length === 0) {
+    console.log("oh no theres no countries");
+  } else {
+    favourites.forEach(renderFavCountryCard);
+  }
+};
+
+const onLoad = () => {
+  const favourites = getFromLocalStorage();
+
+  renderFavouritesCards(favourites);
 };
 
 $(document).ready(onLoad);
