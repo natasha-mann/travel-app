2a9573ff03c733f343fffac913980d5dda1dfd48
Pamela <pamela_bhopal@hotmail.co.uk>
2021-04-19 21:22:38 +0100

    updated placeholder with cityname for card

--- assets/js/results.js
+++ assets/js/results.js
@@ -369,7 +369,7 @@ const renderHealthCard = (countryCardData, travelBriefingData) => {
       `<div class="ui placeholder center aligned segment">
       <div class="ui icon header">
         <i class="medkit icon"></i>
-        There is currently no vaccines data available for this country.
+        There is currently no vaccine data available for ${countryCardData.name}.
       </div>
     </div>`
     );
