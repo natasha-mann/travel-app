b9907c725b06e30ccdbf1bdf92147ed53c3aa371
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-04-19 18:37:50 +0100

    random button added on html and js

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,4 +19,32 @@ const onSubmit = (event) => {
   }
 };
 
+// async await - function to fetch data from api (taking in a url) and returns the data
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getRandomCountry = async () => {
+  const allCountryData = await fetchData(
+    "https://restcountries.eu/rest/v2/all"
+  );
+  console.log(allCountryData);
+};
+
+const randomSearch = async (event) => {
+  event.preventDefault();
+  const randomCountry = await getRandomCountry();
+
+  let index = Math.floor(Math.random() * countries.length - 1);
+  let countryName = countries[index];
+  window.location.href = `/results.html?country=${countryName}`;
+};
+
 $("#start-form").on("submit", onSubmit);
+$("#random-form").on("submit", randomSearch);
